2acf89bff27a8790a4bbb22e3140b5af01eb3046
Hamza <hamzasiddiq755@gmail.com>
2022-01-01 06:10:02 -0800

    Data Science assignment

---
+++ abd.py
@@ -0,0 +1,29 @@
+
+print("Twinkle, twinkle, little star,")
+print(" How I wonder what you are!");
+print(" Up above the world so high,");
+print(" Like a diamond in the sky.");
+print("Twinkle, twinkle, little star,");
+print(" How I wonder what you are");
+
+Import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+
+Import datetime
+Now = datetime.datetime.now()
+print ("Current date and time : ")
+print (now.strftime("%d-%m-%y %H:%M:%S"))
+
+From math import pi
+R = float(input ("Enter radius of circle : "))
+print ("Area of the circle is: " + str(pi * r**2))
+Fname = input("Input your First Name : ")
+Lname = input("Input your Last Name : ")
+print ("Hello " + lname + " " + fname)
+
+A = int(input("enter first number: "))
+B = int(input("enter second number: "))
+Sum = a + b
+print("sum:", sum)
